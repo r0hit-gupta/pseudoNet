dee752afd86ba3291069cb736a33e8f5b5a571f3
r0hit-gupta <rohit1997gupta@gmail.com>
2017-02-11 00:55:18 +0530

    changes

--- index.js
+++ index.js
@@ -6,6 +6,20 @@ var app = express();
 var path = require("path");
 app.use(express.static(__dirname + '/src'));
 
+// FUNCTION TO GET LATTITUDE LONGITUDE FOR A PLACE
+function getCoords(place){
+  // Google Maps API Key
+  const API_KEY = 'AIzaSyAtx_lIJ0GsFLKtlaCsMyo7K7Rq8IeTCx4';
+  var url = 'https://maps.googleapis.com/maps/api/geocode/json?address='+place+'&key='+API_KEY;
+  request.get(url, function(req, res, body){
+    body = JSON.parse(body);
+    return body;
+    return {
+      lat: body.results[0].geometry.location.lat,
+      long: body.results[0].geometry.location.long
+    }
+  });
+}
 
 // ENDPOINT FOR WEATHER
 app.get('/weather/:city', function (req, res) {
@@ -33,7 +47,7 @@ app.get('/nearbuy/:place', function (req, res) {
   const API_KEY = 'AIzaSyCdW8DZofdjeJfGNI6jJ1SP5cj3bABLcnI';
   // Set City
   var place = req.params.place;
-
+  console.log(getCoords(place));
   var url = 'https://maps.googleapis.com/maps/api/place/textsearch/json?key=' + API_KEY + '&query=' + place;
   // Get weather details
   request.get(url, function(request, response, body){
