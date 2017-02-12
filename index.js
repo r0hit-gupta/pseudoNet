618d1629399afc77ceeb398c0753657625015827
r0hit-gupta <rohit1997gupta@gmail.com>
2017-02-12 10:42:11 +0530

    port changes

--- index.js
+++ index.js
@@ -399,6 +399,6 @@ if(cmd == 'action'){
 });
 
 // Server Port Setup
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Now listening on port 3000');
+app.listen(8081, function () {
+  console.log('Now listening on port 8081');
 });
